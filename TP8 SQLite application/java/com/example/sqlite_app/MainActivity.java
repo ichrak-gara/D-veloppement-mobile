6fd6ad827232810fceafbaa9ac7c25b2b6bf7805
Ichrak Gara <72881342+ichrak-gara@users.noreply.github.com>
2021-05-25 23:40:22 +0100

    Add files via upload

---
+++ TP8 SQLite application/java/com/example/sqlite_app/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.sqlite_app;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText name, contact, dob;
+    Button insert, update, delete, view;
+    DBHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name = findViewById(R.id.name);
+        contact = findViewById(R.id.contact);
+        dob = findViewById(R.id.dob);
+        insert = findViewById(R.id.btnInsert);
+        update = findViewById(R.id.btnUpdate);
+        delete = findViewById(R.id.btnDelete);
+        view = findViewById(R.id.btnView);
+        DB = new DBHelper(this);
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameTXT = name.getText().toString();
+                String contactTXT = contact.getText().toString();
+                String dobTXT = dob.getText().toString();
+
+                Boolean checkinsertdata = DB.insertuserdata(nameTXT, contactTXT, dobTXT);
+                if(checkinsertdata==true)
+                    Toast.makeText(MainActivity.this, "Nouvelles données ont été saisies", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Nouvelles données n'ont pas été saisies", Toast.LENGTH_SHORT).show();
+            }        });
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameTXT = name.getText().toString();
+                String contactTXT = contact.getText().toString();
+                String dobTXT = dob.getText().toString();
+
+                Boolean checkupdatedata = DB.updateuserdata(nameTXT, contactTXT, dobTXT);
+                if(checkupdatedata==true)
+                    Toast.makeText(MainActivity.this, "Données modifiée", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Données non modifiée", Toast.LENGTH_SHORT).show();
+            }        });
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameTXT = name.getText().toString();
+                Boolean checkudeletedata = DB.deletedata(nameTXT);
+                if(checkudeletedata==true)
+                    Toast.makeText(MainActivity.this, "Données supprimée", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Données non supprimée", Toast.LENGTH_SHORT).show();
+            }        });
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = DB.getdata();
+                if(res.getCount()==0){
+                    Toast.makeText(MainActivity.this, "aucune entrée existe", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while(res.moveToNext()){
+                    buffer.append("Nom et Prénom :"+res.getString(0)+"\n");
+                    buffer.append("Contact :"+res.getString(1)+"\n");
+                    buffer.append("Numéro de CIN :"+res.getString(2)+"\n\n");
+                }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setCancelable(true);
+                builder.setTitle("Les données enregistrées:"+"\n\n");
+                builder.setMessage(buffer.toString());
+                builder.show();
+            }        });
+    }}
